cf07e30f7121d2ff0f4da270d426f370b2fe6d9e
Zhe Wang <zhewang@Zhes-MacBook-Air.local>
2019-08-31 23:37:03 -0700

    Finished chapter 09

--- play_with_graph_algorithms/chapter09/hamilton_loop.py
+++ play_with_graph_algorithms/chapter09/hamilton_loop.py
@@ -41,6 +41,44 @@ class HamiltonLoop:
         return res[::-1]
 
 
+class HamiltonLoopV2:
+    """With 'visited' bitwise optimization"""
+    def __init__(self, G):
+        self._G = G
+        self._pre = [0] * G.V
+        self._end = -1
+        self._dfs(0, 0, 0, G.V)
+
+    def _dfs(self, visited, v, parent, left):
+        # visited at bit v is 0
+        visited += (1 << v)
+        self._pre[v] = parent
+        left -= 1
+        if left == 0 and self._G.has_edge(v, 0):
+            self._end = v
+            return True
+
+        for w in self._G.adj(v):
+            if (visited & (1 << w)) == 0:
+                if self._dfs(visited, w, v, left):
+                    return True
+
+        return False
+
+    def result(self):
+        res = []
+        if self._end == -1:
+            return res
+        
+        curr = self._end
+        while curr != 0:
+            res.append(curr)
+            curr = self._pre[curr]
+        res.append(0)
+
+        return res[::-1]
+
+
 if __name__ == '__main__':
     filename = 'play_with_graph_algorithms/chapter09/g.txt'
     graph = Graph(filename)
@@ -51,3 +89,13 @@ if __name__ == '__main__':
     graph = Graph(filename)
     hamilton_loop = HamiltonLoop(graph)
     print(hamilton_loop.result())
+
+    filename = 'play_with_graph_algorithms/chapter09/g.txt'
+    graph = Graph(filename)
+    hamilton_loop_v2 = HamiltonLoopV2(graph)
+    print(hamilton_loop_v2.result())
+
+    filename = 'play_with_graph_algorithms/chapter09/g2.txt'
+    graph = Graph(filename)
+    hamilton_loop_v2 = HamiltonLoopV2(graph)
+    print(hamilton_loop_v2.result())
--- play_with_graph_algorithms/chapter09/hamilton_path.py
+++ play_with_graph_algorithms/chapter09/hamilton_path.py
@@ -42,6 +42,44 @@ class HamiltonPath:
         return res[::-1]
 
 
+class HamiltonPathV2:
+
+    def __init__(self, G, s):
+        self._G = G
+        self._s = s
+        self._pre = [0] * G.V
+        self._end = -1
+        self._dfs(0, s, s, G.V)
+
+    def _dfs(self, visited, v, parent, left):
+        visited += (1 << v)
+        self._pre[v] = parent
+        left -= 1
+        if left == 0:
+            self._end = v
+            return True
+
+        for w in self._G.adj(v):
+            if (visited & (1 << w)) == 0:
+                if self._dfs(visited, w, v, left):
+                    return True
+
+        return False
+
+    def result(self):
+        res = []
+        if self._end == -1:
+            return res
+        
+        curr = self._end
+        while curr != self._s:
+            res.append(curr)
+            curr = self._pre[curr]
+        res.append(self._s)
+
+        return res[::-1]
+
+
 if __name__ == '__main__':
     filename = 'play_with_graph_algorithms/chapter09/g.txt'
     graph = Graph(filename)
@@ -52,3 +90,13 @@ if __name__ == '__main__':
     graph = Graph(filename)
     hamilton_path = HamiltonPath(graph, 1)
     print(hamilton_path.result())
+
+    filename = 'play_with_graph_algorithms/chapter09/g.txt'
+    graph = Graph(filename)
+    hamilton_path_v2 = HamiltonPathV2(graph, 0)
+    print(hamilton_path_v2.result())
+
+    filename = 'play_with_graph_algorithms/chapter09/g2.txt'
+    graph = Graph(filename)
+    hamilton_path_v2 = HamiltonPathV2(graph, 1)
+    print(hamilton_path_v2.result())
